2affe8d2ac11d1e71b39d3b5aa7c5753d7c3106b
daspro-obsheet2 <wildanwibisono52@gmail.com>
2025-02-22 06:38:19 +0700

    Tugas nomer 1

--- MataKuliah14.java
+++ MataKuliah14.java
@@ -4,21 +4,21 @@ public class MataKuliah14 {
     int sks;
     int jumlahJam;
 
-    public MataKuliah14() {   // Konstruktor default
+    public MataKuliah14() {   
         this.kodeMK = "001";
         this.nama = "Bahasa Indonesia";
         this.sks = 4;
         this.jumlahJam = 8;
     }
 
-    public MataKuliah14(String kodeMK, String nama, int sks, int jumlahJam) {    // Konstruktor berparameter
+    public MataKuliah14(String kodeMK, String nama, int sks, int jumlahJam) {    
         this.kodeMK = kodeMK;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
 
-    public void tampilInformasi() {   // Method untuk menampilkan informasi mata kuliah
+    public void tampilInformasi() {   
         System.out.println("Kode Matkul : " + kodeMK);
         System.out.println("Nama Matkul : " + nama);
         System.out.println("SKS         : " + sks);
@@ -26,17 +26,17 @@ public class MataKuliah14 {
         System.out.println("------------------------------");
     }
 
-    public void ubahSKS(int sksBaru) {  // Method untuk mengubah SKS
+    public void ubahSKS(int sksBaru) {  
         this.sks = sksBaru;
         System.out.println("SKS diubah menjadi: " + sks);
     }
 
-    public void tambahJam(int jam) { // Method untuk menambah jam
+    public void tambahJam(int jam) { 
         this.jumlahJam += jam;
         System.out.println("Jumlah jam ditambah menjadi: " + jumlahJam);
     }
 
-    public void kurangiJam(int jam) {  // Method untuk mengurangi jam
+    public void kurangiJam(int jam) {  
         
         if (jam > jumlahJam) {
             System.out.println("------------------------------");
